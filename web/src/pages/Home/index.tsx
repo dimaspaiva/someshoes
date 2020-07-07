788423a78e05f2e528b505c70b93b6fec0492858
dimaspaiva <dimasalpaiva@gmail.com>
2020-07-07 17:55:54 -0300

    increment products list

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,31 +1,54 @@
 import React, { useState } from 'react'
+
 import Header from '../../components/Header'
+import Product from '../../components/Product'
 
 import './styles.css'
 
 const Home = () => {
-  const [success, setSuccess] = useState(false)
+  const [success, setSuccess] = useState(true)
+  const [request, setRequest] = useState(false)
+
+  const generator = () => {
+    const productArray = []
+
+    for (let i = 0; i < 15; i++) {
+      productArray.push(<Product />)
+    }
+
+    return productArray
+  }
 
   return (
-    <div>
+    <>
       <Header />
-      {success ? (
-        <div className="slogan-container">
+      {request && (
+        <section className="slogan-container">
           <h2 className="slogan-title ">
             A maior loja online de artigos esportivos
           </h2>
           <h3 className="slogan-subtitle ">Procurou, achou, comprou!</h3>
-        </div>
+        </section>
+      )}
+      {success ? (
+        <section>
+          <div className="search-results">
+            <p className="search-text">"Camisa Íbis"</p>
+            <p className="search-amount">1 - 15 | 99 resultados</p>
+          </div>
+
+          <div className="product-list">{generator()}</div>
+        </section>
       ) : (
-        <div className="warning-container">
+        <section className="warning-container">
           <p>
             Desculpe não encontramos o produto{' '}
             <strong>"Camisa do mundial do Palmeiras"</strong> em nossa
             lista. :/
           </p>
-        </div>
+        </section>
       )}
-    </div>
+    </>
   )
 }
 
