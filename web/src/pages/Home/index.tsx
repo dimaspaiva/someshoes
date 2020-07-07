d9db3abe61f16a272e6902fe003bb8dc2361194f
dimaspaiva <dimasalpaiva@gmail.com>
2020-07-07 16:33:14 -0300

    add warning

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 
 import './styles.css'
 
 const Home = () => {
+  const [success, setSuccess] = useState(false)
+
   return (
     <div>
       <Header />
-      <div className="slogan-container">
-        <h2 className="slogan-title ">
-          A maior loja online de artigos esportivos
-        </h2>
-        <h3 className="slogan-subtitle ">Procurou, achou, comprou!</h3>
-      </div>
+      {success ? (
+        <div className="slogan-container">
+          <h2 className="slogan-title ">
+            A maior loja online de artigos esportivos
+          </h2>
+          <h3 className="slogan-subtitle ">Procurou, achou, comprou!</h3>
+        </div>
+      ) : (
+        <div className="warning-container">
+          <p>
+            Desculpe não encontramos o produto{' '}
+            <strong>"Camisa do mundial do Palmeiras"</strong> em nossa
+            lista. :/
+          </p>
+        </div>
+      )}
     </div>
   )
 }
