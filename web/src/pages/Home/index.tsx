6ac96090ede50dfa0d401a0c5b23d5ecc170c211
dimaspaiva <dimasalpaiva@gmail.com>
2020-07-08 16:50:03 -0300

    request product from server

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,19 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import api from '../../services/api'
 
 import Header from '../../components/Header'
 import Product from '../../components/Product'
 
 import './styles.css'
 
+interface Product {
+  id: number
+  title: string
+  description: string
+  avaibleSizes: string[]
+  style: string
+  prince: number
+  isFreeShipping: boolean
+}
+
+interface Data {
+  message: string
+  products: Product[]
+  amount: number
+}
+
 const Home = () => {
-  const [success, setSuccess] = useState(true)
+  const [success, setSuccess] = useState(false)
   const [request, setRequest] = useState(false)
+  const [products, setProducts] = useState<Product[]>([])
+  const [amount, setAmount] = useState(0)
+
+  useEffect(() => {
+    api.get('products').then(({ data }) => {
+      const { products, amount } = data
+      setProducts(products)
+      setAmount(amount)
+    })
+  }, [])
 
   const generator = () => {
     const productArray = []
 
     for (let i = 0; i < 15; i++) {
-      productArray.push(<Product />)
+      productArray.push(<Product key={i} />)
     }
 
     return productArray
@@ -22,22 +50,19 @@ const Home = () => {
   return (
     <>
       <Header />
-      {request && (
+      {!request ? (
         <section className="slogan-container">
           <h2 className="slogan-title ">
             A maior loja online de artigos esportivos
           </h2>
           <h3 className="slogan-subtitle ">Procurou, achou, comprou!</h3>
         </section>
-      )}
-      {success ? (
+      ) : success ? (
         <section>
           <div className="search-results">
             <p className="search-text">"Camisa Íbis"</p>
             <p className="search-amount">1 - 15 | 99 resultados</p>
           </div>
-
-          <div className="product-list">{generator()}</div>
         </section>
       ) : (
         <section className="warning-container">
@@ -48,6 +73,13 @@ const Home = () => {
           </p>
         </section>
       )}
+      <section>
+        <div className="product-list">
+          {products.map((product) => (
+            <Product />
+          ))}
+        </div>
+      </section>
     </>
   )
 }
